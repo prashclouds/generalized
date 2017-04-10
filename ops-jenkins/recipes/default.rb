0a0211775a306eca83589d4aa13b344c2f62342f
Abel Gutierrez <vegetalink333@gmail.com>
2017-04-10 14:26:18 -0600

    new cookbooks

--- ops-jenkins/recipes/default.rb
+++ ops-jenkins/recipes/default.rb
@@ -12,17 +12,20 @@ include_recipe 'awscli'
 package 'jq'
 
 jenkins_plugin 'git' do
-  notifies :restart, 'service[jenkins]', :immediately
+  #notifies :restart, 'service[jenkins]', :immediately
+  action :install
   ignore_failure true
 end
 
 jenkins_plugin 'git-client'  do
-  notifies :restart, 'service[jenkins]', :immediately
+  #notifies :restart, 'service[jenkins]', :immediately
+  action :install
   ignore_failure true
 end
 
 jenkins_plugin 'slack' do
-  notifies :restart, 'service[jenkins]', :immediately
+  #notifies :restart, 'service[jenkins]', :immediately
+  action :install
   ignore_failure true
 end
 
