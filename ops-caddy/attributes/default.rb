8094814b634895d4eac9d283e9914464b7ee6baa
Abel Rodrigo Gutierrez de Leon <vegetalink333@gmail.com>
2017-04-10 13:57:07 -0600

    Update caddy url

--- ops-caddy/attributes/default.rb
+++ ops-caddy/attributes/default.rb
@@ -1,5 +1,5 @@
 default['caddy']['user'] = 'caddy'
 default['caddy']['group'] = 'caddy'
-default['caddy']['url'] = 'https://caddyserver.com/download/builds/160112192859998/caddy_linux_amd64_custom.tar.gz'
+default['caddy']['url'] = 'https://caddyserver.com/download/build?os=linux&arch=amd64&features='
 default['caddy']['proxy'] = {}
 default['caddy']['email'] = 'vinh.nguyen@nclouds.com'
