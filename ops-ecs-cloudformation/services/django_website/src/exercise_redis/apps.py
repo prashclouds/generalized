cc309ded1a319111ffb68941e3a6391c0468e2bf
fernando <fernando@nclouds.com>
2018-03-22 10:32:13 -0600

    adding ARN IAM user to ecr

--- ops-ecs-cloudformation/services/django_website/src/exercise_redis/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExerciseRedisConfig(AppConfig):
-    name = 'exercise_redis'
--- ops-ecs-cloudformation/services/django_website/src/exercise_redis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^home', views.HomePageView.as_view()),
-    url(r'^elements', views.elements),
-]
