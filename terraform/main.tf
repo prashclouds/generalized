a0c7f12692ae67e463ead343160fb9ce48124ab7
bryanrhm <bryan.rhm1@gmail.com>
2019-01-15 15:31:15 -0600

    added vars to conf/

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,10 @@ terraform {
 
 
 module "vpc" {
-  source          = "modules/vpc"
+  source = "modules/vpc"
   environment = "${var.environment}"
+  vpc = "${var.vpc}"
+  cluster_name = "${var.cluster_name}"
+  public_subnets = "${var.public_subnets}"
+  private_subnets = "${var.private_subnets}"
 }
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,44 +1,10 @@
-variable "vpc" {
-  type        = "map"
-  description = "Map of AWS VPC settings"
+variable "vpc" {}
 
-  default = {
-    cidr          = "192.168.100.0/24"
-    dns_hostnames = true
-    dns_support   = true
-    tag           = ""
-    tenancy       = "default"
-  }
-}
+variable "environment" {}
 
-variable "environment" {
-  type    = "string"
-  default = "dev"
-}
+variable "cluster_name" {}
 
-variable "cluster-name" {
-  type    = "string"
-  default = "unitq"
-}
+variable "public_subnets" {}
 
-variable "public_subnets" {
-  type        = "map"
-  description = "Map of AWS availability zones (key) to subnet CIDR (value) assignments"
-
-  default = {
-    us-east-1a = "192.168.100.0/27"
-    #us-east-1b = "192.168.100.128/27"
-  }
-}
-
-variable "private_subnets" {
-  type        = "map"
-  description = "Map of AWS availability zones (key) to subnet CIDR (value) assignments"
-
-  default = {
-    us-east-1a = "192.168.100.32/27"
-    us-east-1b = "192.168.100.64/27"
-    us-east-1c = "192.168.100.96/27"
-  }
-}
+variable "private_subnets" {}
 
