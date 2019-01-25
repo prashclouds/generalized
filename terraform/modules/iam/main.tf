28f3423574bdbfdaec7fda0d611b461098f591c6
bryanrhm <bryan.rhm1@gmail.com>
2019-01-24 19:28:06 -0600

    fixed remote backend from utility

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -43,7 +43,7 @@ data "aws_iam_policy_document" "mlservice_policy" {
       actions = [
           "kinesis:*",
       ]
-      resources = ["arn:aws:kinesis:${data.aws_region.name}:${data.aws_caller_identity.current.account_id}:stream/${var.environment}*"]
+      resources = ["arn:aws:kinesis:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stream/${var.environment}*"]
   }
 }
 
