01358d4fd18367d55e02e0b5bfc5fe9b18da50f4
Ronaldo Umana <ronaldo.ur@gmail.com>
2019-02-28 15:16:12 -0600

    Added vulnerability scanner and fixed automated AMI process

--- cloudformation/ECS/commands/sync.sh
+++ cloudformation/ECS/commands/sync.sh
@@ -2,4 +2,4 @@ cd "${BASH_SOURCE%/*}" || exit
 cd ..
 COMMAND=$(sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' config.yml)
 eval $COMMAND
-aws s3 sync . "s3://$TemplatesBucketName/$Prefix" --exclude "*" --include "*.yml"
+aws s3 sync . "s3://$TemplatesBucketName/$Prefix" --exclude "*" --include "*.yml" --include "*.zip"
